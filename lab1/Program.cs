6afb18213b135a29808145a42c4445a398c7d0c3
dezmerek <mszymanski027@gmail.com>
2022-10-11 15:04:18 +0200

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arrl = { 23, 1, 56, 345, 1, 5, 67, 11 };
+        int index = FindTwoDigitMin(arrl);
+        if (index == 7)
+        {
+            Console.WriteLine("Ok");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+        int[] arr2 = { };
+        index = FindTwoDigitMin(arr2);
+        if (index == -1)
+        {
+            Console.WriteLine("Ok");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+        int[] arr3 = { 1, 3, 4, 123, 1234 };
+        index = FindTwoDigitMin(arr3);
+        if (index == -1)
+        {
+            Console.WriteLine("Ok");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+
+    ///<summary>
+
+    public static int FindTwoDigitMin(int[] arr)
+    {
+        int min = 100;
+        int index = -1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < min && arr[i] > 9)
+            {
+                min = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
